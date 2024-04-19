2f4cfc524185c3a3de260b1134a87eb07eeec370
Sławek Kasperowicz <slawek.kasperowicz@gmail.com>
2024-04-19 18:37:32 +0200

    add dictionary

---
+++ task.py
@@ -0,0 +1,7 @@
+shopping_dict = {
+    "piekarnia": ['chleb', 'pączek', 'bułki'],
+    "warzywniak": ['marchew', 'seler', 'rukola'],
+}
+for i in shopping_dict:
+    print(f"Idę do {i} i kupuję tam {shopping_dict[i]}")
+    
